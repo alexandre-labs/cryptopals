b9345ea16c6f8967c1384003180f55c1672e27a6
Alexandre <0x41e@protonmail.ch>
2018-05-07 07:02:51 -0300

    Create challenge3 files

---
+++ cryptopals/set1/challenge3.py
@@ -0,0 +1,35 @@
+"""
+
+Single-byte XOR cipher
+
+The hex encoded string:
+
+1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+
+... has been XOR'd against a single character. Find the key, decrypt the message.
+
+You can do this by hand. But don't: write code to do it for you.
+
+How? Devise some method for "scoring" a piece of English plaintext. Character frequency is a good metric.
+Evaluate each output and choose the one with the best score.
+Achievement Unlocked
+
+You now have our permission to make "ETAOIN SHRDLU" jokes on Twitter.
+"""
+from operator import xor
+
+from cryptopals import utils
+
+
+def decrypt_message(hex_string: str) -> str:
+
+    # The underscore would be the score. 'score' means how many times this hex appears in the string...
+    most_frequent_hex, _ = utils.get_most_frequent_hex(hex_string)
+
+    # mf_ -> most frequent
+    # Getting the ascii letter related to the most frequent hex, but in uppercase
+    mf_ascii_int = ord(chr(int(most_frequent_hex, 16)).upper())
+
+    int_chunks = utils.split_hex_string_into_int_chunks(hex_string)
+
+    return ''.join(chr(xor(mf_ascii_int, chunk)) for chunk in int_chunks)
--- cryptopals/utils.py
+++ cryptopals/utils.py
@@ -1,4 +1,23 @@
+import collections
+import random
+import string
 from typing import Tuple
+
+
+def generate_random_hex_chunks(chunk_size: int = 2) -> str:
+
+    if not(chunk_size % 2 == 0):
+        raise ValueError('Chunk size should be a multiple of 2.')
+
+    return ''.join(random.choices(string.hexdigits, k=chunk_size))
+
+
+def generate_random_hex_strings(chunk_size:int = 2, string_size:int  = 256) -> str:
+
+    if not all([chunk_size % 2 == 0, string_size % 2 == 0]):
+        raise ValueError('Chunk size and string_size should be a multiple of 2.')
+
+    return ''.join(generate_random_hex_chunks(chunk_size) for n in range(0,  int(string_size / chunk_size)))
 
 
 def split_string_into_chunks(string: str, chunk_size: int) -> Tuple[str, ...]:
@@ -7,3 +26,11 @@ def split_string_into_chunks(string: str, chunk_size: int) -> Tuple[str, ...]:
 
 def split_hex_string_into_int_chunks(hex_string: str) -> Tuple[int, ...]:
     return tuple(int(chunk, 16) for chunk in split_string_into_chunks(hex_string, 2))
+
+
+def score_hex_string(hex_string: str) -> collections.Counter:
+    return collections.Counter(split_string_into_chunks(hex_string, 2))
+
+
+def get_most_frequent_hex(hex_string: str) -> Tuple[str, int]:
+    return max(score_hex_string(hex_string).items(), key=lambda pair: pair[1])
